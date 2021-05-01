b0de7a5f717e245e08aff2987b854115cb7d8d95
Dimitar <mbarantiev@gmail.com>
2021-05-01 18:05:05 +0300

    Add Typescript support to Node.js

--- server/app.js
+++ server/app.js
@@ -1,16 +1,13 @@
-'use strict';
-
-const express = require('express');
-
-// Constants
-const PORT = 8080;
-const HOST = '0.0.0.0';
-
-// App
-const app = express();
-app.get('/', (req, res) => {
-  res.send('TEST 1234');
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var express_1 = __importDefault(require("express"));
+var app = express_1.default();
+app.get('/', function (req, res) {
+    res.send('Well done!');
 });
-
-app.listen(PORT, HOST);
-console.log(`Running on http://${HOST}:${PORT}`);
+app.listen(3000, function () {
+    console.log('The application is listening on port 3000!');
+});
