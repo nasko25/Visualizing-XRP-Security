86516cc6da8c8b9e9dbdc0ee0e3853160b83dca5
NikolayBlagoev <N.Blagoev@student.tudelft.nl>
2021-05-12 16:50:18 +0300

    Added the GeoLocator... GeoLocater? The GeoDude, whatever

---
+++ server/geoLocate.ts
@@ -0,0 +1,66 @@
+import axios from 'axios';
+
+
+var testData: string[] = ['91.12.98.74', '209.195.2.50', '194.35.86.10']
+
+class GeoLocate{
+    IPList: string[];
+
+    constructor(IPs: string[]){
+        this.IPList = IPs;
+    }
+
+    setIPList(newList: string[]){
+        this.IPList = newList;
+    }
+
+    async wait(seconds: number){
+
+        return new Promise(resolve => setTimeout(resolve, seconds*1000));
+    }
+
+    async getData(ip: string) {
+        //access key needed for ipstack api
+        const accessKey: string = '1f7dce5f41b09991c3b932423e359995';
+        try {
+           let response = await axios({
+                url: 'http://api.ipstack.com/' + ip + '?access_key=' + accessKey,
+                method: 'get',
+                timeout: 8000
+            })
+            if(response.status == 200){
+                return [response.data.latitude, response.data.longitude];
+            }     
+            throw new Error('response was not 200');    //sad moments here
+        }catch (err) {
+            throw new Error(err);
+        }
+    }
+    //Recursively call from callback. This is probably forbidden by the Geneva Convention, but no other way to do it.
+    locateHelper(curr: number){
+        if(curr >= this.IPList.length){
+            return;
+        }
+        this.getData(this.IPList[curr]).then(res => {
+            console.log(res);
+            //Delay requests by 1 second, so to not get blocked by the API
+        
+            this.wait(1).then((res) => this.locateHelper(curr+1)); 
+        })
+    }
+
+    locate(){
+        if(this.IPList == null || this.IPList.length == 0){
+            return;
+        }
+        
+        try{
+        this.locateHelper(0);
+        } catch(e){
+            console.log(e);
+        }
+    }
+
+}
+
+export default GeoLocate;
