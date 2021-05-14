76deace2a7e6ccf2213cc90d4a68558fd8d88692
Atanas Pashov <a.i.pashov@student.tudelft.nl>
2021-05-14 18:43:48 +0300

    Added a library that dynamically loads only the visible nodes from a list of nodes

--- client/src/components/DashboardList.jsx
+++ client/src/components/DashboardList.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 import ListGroup from 'react-bootstrap/ListGroup';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import { MDBContainer } from 'mdbreact';
+import {List, InfiniteLoader, AutoSizer} from 'react-virtualized';
 
 export default class DashboardList extends Component {
     nodes = [];
@@ -12,17 +13,32 @@ export default class DashboardList extends Component {
         this.data = this.props.data;
         this.state = {nodes: []};
 
-        this.createList();
     }
     componentDidMount() {
+        this.createList();
     }
+
+    _noRowsRenderer() {
+        return <div >No rows</div>;
+    }
+    _rowRenderer({index, isScrolling, key, style}) {
+        return (
+            <div style = {style} key = {key} >
+                <span className="ip">IP {index} </span>
+                    <span className="version">Version</span>
+                    <span className="public_key">Public Key</span>
+                    <span className="uptime">Uptime</span>
+                    <span className="security_metric">Security Metric</span>
+            </div>
+        );
+  }
 
     createList() {
         console.log("data")
         console.log(this.data)
         this.data.then(data => {
             console.log("here" + data[0].IP)
-        for (let i=0; i< data.length; i++) {
+        /*for (let i=0; i< data.length; i++) {
             let updatedNodes = this.state.nodes.concat(<ListGroup.Item>
                     <span className="ip">IP</span>
                     <span className="version">Version</span>
@@ -30,24 +46,24 @@ export default class DashboardList extends Component {
                     <span className="uptime">Uptime</span>
                     <span className="security_metric">Security Metric</span>
                 </ListGroup.Item>);
-            this.setState({nodes: updatedNodes });
-            }
+            this.setState({nodes: updatedNodes });*/
+            this.setState({nodes: data});
+            //}
         });
     }
 
     render() {
         return (
-            <div className='list'>
-                <div className='list_inner'>
-                    <MDBContainer>
-                        <div className="scrollbar scrollbar-primary  mt-5 mx-auto">
-                            <ListGroup className='list_group'>
-                                {this.state.nodes}
-                            </ListGroup>
-                        </div>
-                    </MDBContainer>
-                </div>
-            </div>
+            <List
+                ref = "List"
+    rowRenderer={this._rowRenderer}
+                rowCount={this.state.nodes.length}
+                height={300}
+                rowHeight={40}
+                width= {600}
+                overscanRowCount = {10}
+    noRowsRenderer={this._noRowsRenderer}
+            />
         )
     }
 }
