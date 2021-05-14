fc20a1c108274073b52be6fa473711c78ab65b60
istranski <I.I.Stranski@student.tudelft.nl>
2021-05-14 11:02:54 +0300

    fill the list based on the contents of an array

--- client/src/components/DashboardList.jsx
+++ client/src/components/DashboardList.jsx
@@ -4,6 +4,18 @@ import ListGroup from 'react-bootstrap/ListGroup';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import { MDBContainer } from 'mdbreact';
 
+let nodes = [];
+
+for (let i=0; i<15; i++) {
+    nodes.push(<ListGroup.Item>
+            <span className="ip">IP</span>
+            <span className="version">Version</span>
+            <span className="public_key">Public Key</span>
+            <span className="uptime">Uptime</span>
+            <span className="security_metric">Security Metric</span>
+        </ListGroup.Item>);
+}
+
 export default class DashboardList extends Component {
     data = [];
 
@@ -13,99 +25,18 @@ export default class DashboardList extends Component {
         this.data = data;
     }
 
+    
+
     render() {
         return (
             <div className='list'>
                 <div className='list_inner'>
                     <MDBContainer>
-                    <div className="scrollbar scrollbar-primary  mt-5 mx-auto">
-                    <ListGroup className='list_group'>
-                        <ListGroup.Item>
-                        <span className="ip">IP</span>
-                            <span className="version">Version</span>
-                            <span className="public_key">Public Key</span>
-                            <span className="uptime">Uptime</span>
-                            <span className="security_metric">Security Metric</span>
-                        </ListGroup.Item>
-                        <ListGroup.Item>
-                        <span className="ip">IP</span>
-                            <span className="version">Version</span>
-                            <span className="public_key">Public Key</span>
-                            <span className="uptime">Uptime</span>
-                            <span className="security_metric">Security Metric</span>
-                        </ListGroup.Item>
-                        <ListGroup.Item>
-                        <span className="ip">IP</span>
-                            <span className="version">Version</span>
-                            <span className="public_key">Public Key</span>
-                            <span className="uptime">Uptime</span>
-                            <span className="security_metric">Security Metric</span>
-                        </ListGroup.Item>
-                        <ListGroup.Item>
-                        <span className="ip">IP</span>
-                            <span className="version">Version</span>
-                            <span className="public_key">Public Key</span>
-                            <span className="uptime">Uptime</span>
-                            <span className="security_metric">Security Metric</span>
-                        </ListGroup.Item>
-                        <ListGroup.Item>
-                        <span className="ip">IP</span>
-                            <span className="version">Version</span>
-                            <span className="public_key">Public Key</span>
-                            <span className="uptime">Uptime</span>
-                            <span className="security_metric">Security Metric</span>
-                        </ListGroup.Item>
-                        <ListGroup.Item>
-                        <span className="ip">IP</span>
-                            <span className="version">Version</span>
-                            <span className="public_key">Public Key</span>
-                            <span className="uptime">Uptime</span>
-                            <span className="security_metric">Security Metric</span>
-                        </ListGroup.Item>
-                        <ListGroup.Item>
-                        <span className="ip">IP</span>
-                            <span className="version">Version</span>
-                            <span className="public_key">Public Key</span>
-                            <span className="uptime">Uptime</span>
-                            <span className="security_metric">Security Metric</span>
-                        </ListGroup.Item>
-                        <ListGroup.Item>
-                        <span className="ip">IP</span>
-                            <span className="version">Version</span>
-                            <span className="public_key">Public Key</span>
-                            <span className="uptime">Uptime</span>
-                            <span className="security_metric">Security Metric</span>
-                        </ListGroup.Item>
-                        <ListGroup.Item>
-                        <span className="ip">IP</span>
-                            <span className="version">Version</span>
-                            <span className="public_key">Public Key</span>
-                            <span className="uptime">Uptime</span>
-                            <span className="security_metric">Security Metric</span>
-                        </ListGroup.Item>
-                        <ListGroup.Item>
-                        <span className="ip">IP</span>
-                            <span className="version">Version</span>
-                            <span className="public_key">Public Key</span>
-                            <span className="uptime">Uptime</span>
-                            <span className="security_metric">Security Metric</span>
-                        </ListGroup.Item>
-                        <ListGroup.Item>
-                        <span className="ip">IP</span>
-                            <span className="version">Version</span>
-                            <span className="public_key">Public Key</span>
-                            <span className="uptime">Uptime</span>
-                            <span className="security_metric">Security Metric</span>
-                        </ListGroup.Item>
-                        <ListGroup.Item>
-                        <span className="ip">IP</span>
-                            <span className="version">Version</span>
-                            <span className="public_key">Public Key</span>
-                            <span className="uptime">Uptime</span>
-                            <span className="security_metric">Security Metric</span>
-                        </ListGroup.Item>
-                    </ListGroup>
-                    </div>
+                        <div className="scrollbar scrollbar-primary  mt-5 mx-auto">
+                            <ListGroup className='list_group'>
+                                {nodes}
+                            </ListGroup>
+                        </div>
                     </MDBContainer>
                 </div>
             </div>
