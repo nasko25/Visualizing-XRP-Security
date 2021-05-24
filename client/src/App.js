05ea1ba51dc41f6ffe823a131e6776c482acdcce
istranski <I.I.Stranski@student.tudelft.nl>
2021-05-24 13:50:13 +0300

    Create a separate folder for the Dashboard in Components

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import Dashboard from "./components/Dashboard";
+import Dashboard from "./components/dashboard/Dashboard";
 import NodePageMain from './components/node-page/NodePageMain';
 
 function App() {
--- client/src/components/dashboard/Dashboard.tsx
+++ client/src/components/dashboard/Dashboard.tsx
@@ -2,11 +2,11 @@ import { Component } from "react";
 import React from 'react';
 import DashboardNavbar from "./DashboardNavbar";
 import DashboardList from "./DashboardList";
-import TopMap from "./TopMap";
+import TopMap from "../TopMap";
 import axios from 'axios';
 
-import NodePageMain from "./node-page/NodePageMain";
-let dataJson = require("../nodes.json");
+import NodePageMain from "../node-page/NodePageMain";
+let dataJson = require("../../nodes.json");
 
 export default class Dashboard extends Component {
     // Hardcoded data for example purposes for the midterm presentaion
