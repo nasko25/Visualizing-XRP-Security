eba54bf5c22495e3a38e4e45fe88677fd9e2e76c
Marko Ivanov <m.v.ivanov@student.tudelft.nl>
2021-05-27 20:57:18 +0300

    Remove redundant API endpoints that were left for testing

--- server/app.ts
+++ server/app.ts
@@ -36,7 +36,6 @@ async function startCrawler() {
 }
 
 async function startPortScanner() {
-
     let portScanner = new PortScanner();
     portScanner.start()
 }
@@ -61,53 +60,6 @@ repeated_crawl();
 // startPortScanner().catch((e) => {
 //     console.log(`Crawler exited with the exception: ${e}.`);
 // });
-
-app.get('/insert-node', (req, res) => {
-    var n: CrawlerNode = {ip: '127.0.0.1', port: 51235, version: '1.7.0', pubkey: 'pk', uptime: 10};
-    insertNode(n);
-    res.send("node inserted");
-});
-
-app.get("/insert-sas", (req, res) => {
-    var sa: SecurityAssessment = {
-        public_key: "pub_key_1",
-        metric_version: 0.1,
-        score: 1,
-    };
-    insertSecurityAssessment(sa);
-    res.send("Security assessment inserted.");
-});
-
-app.get("/insert-connection", (req, res) => {
-    var start_node: CrawlerNode = {
-        ip: "127.0.0.1",
-        port: 51235,
-        version: "1.7.0",
-        pubkey: "pk",
-        uptime: 10,
-    };
-    var end_node: CrawlerNode = {
-        ip: "127.0.0.1",
-        port: 51235,
-        version: "1.7.0",
-        pubkey: "pk",
-        uptime: 10,
-    };
-    insertConnection(start_node, end_node);
-    res.send("connection inserted");
-});
-
-app.get("/get-all-connections", (req, res) => {
-    var nodes = getAllConnections(function (result): void {
-        res.send(JSON.stringify(result));
-    });
-});
-
-app.get("/get-all-sas", (req, res) => {
-    var nodes = getAllSecurityAssessments(function (result): void {
-        res.send(JSON.stringify(result));
-    });
-});
 
 // Add the Client API Endpoints to the server
 setupClientAPIEndpoints(app);
