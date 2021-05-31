b07f601650c12575b2dc3b5ee788327178d3d344
istranski <I.I.Stranski@student.tudelft.nl>
2021-05-31 19:17:22 +0300

    Removed unnecessary button in Dashboard

--- client/src/components/dashboard/DashboardNavbar.tsx
+++ client/src/components/dashboard/DashboardNavbar.tsx
@@ -9,30 +9,14 @@ export default class DashboardNavbar extends Component {
             <Grid
                 style={{ width: "100%", height: "100%" }}
                     rows={["1"]}
-                    columns={["1/2", "1/6", "1/6", "1/6"]}
+                    columns={["1/2", "1/4", "1/4"]}
                     areas={[
                         { name: 'title', start: [0, 0], end: [0, 0] },
-                        { name: 'button_stock', start: [1, 0], end: [1, 0] },
-                        { name: 'button_validator', start: [2, 0], end: [2, 0] },
-                        { name: 'button_about', start: [3,0], end: [3,0]}
+                        { name: 'button_validator', start: [1, 0], end: [1, 0] },
+                        { name: 'button_about', start: [2,0], end: [2,0]}
                     ]}
             >
                 <Heading margin="2%" gridArea="title" alignSelf="center" size="small" color='#f8f8f8'>CISELab</Heading>
-
-                <Box
-                    height="80%"
-                    gridArea="button_stock"
-                    justify="center"
-                    alignSelf="center"
-                    margin="2%">
-                    <Button
-                        variant="dark"
-                        style={{ width: "80%", height: "80%", alignSelf: "center" }} >
-                        <Link to='/' className='link' style={{textDecoration: 'none', color: 'inherit'}}>
-                            <Text contentEditable="false" size="large" weight="bold">Stock Nodes</Text>
-                        </Link>
-                    </Button>
-                </Box>
 
                 <Box
                     height="80%"
