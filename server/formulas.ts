5d2c095c87e2a215fd9003760fdbba3a43f221cb
Nikolay Blagoev <n.blagoev@student.tudelft.nl>
2021-06-02 16:24:10 +0300

    Finalised the version calculation

---
+++ server/formulas.ts
@@ -0,0 +1,37 @@
+const def_a: number = 1.1;
+const def_b: number = 1.05;
+const def_c: number = -1.1;
+const def_cutoff: number = -1000;
+const def_decimals: number = 2;
+
+export function power_function(x: number, a?: number, b?: number, c?: number, cutoff?: number, decimals?: number){
+    if(cutoff==undefined) cutoff=def_cutoff;
+    if(a==undefined) a=def_a;
+    if(b==undefined) b=def_b;
+    if(c==undefined) c=def_c;
+    if(decimals==undefined) decimals=def_decimals;
+    
+    return Math.max(cutoff,round_to_decimals(100-100*(a*(Math.pow(b,x))+c), decimals)) 
+}
+
+
+
+export function quadratic_function(x: number, a?: number, b?: number, c?: number, cutoff?: number, decimals?: number){
+    if(cutoff==undefined) cutoff=def_cutoff;
+    if(a==undefined) a=-0.002;
+    if(b==undefined) b=0;
+    if(c==undefined) c=1;
+    if(decimals==undefined) decimals=def_decimals;
+    
+    return Math.max(cutoff,100*((a*(x*x))+b*x+c));
+}
+export function round_to_decimals(x: number, decimals: number){
+    return Math.round((x+Number.EPSILON)*Math.pow(10,decimals))/Math.pow(10,decimals)
+} 
+
+// console.log(round_to_decimals(4.14141,1));
+// console.log(round_to_decimals(4525.1,4));
+// console.log(round_to_decimals(6262.00414141,3));
+// console.log(power_function(0));
+// console.log(quadratic_function(10));
+// console.log(quadratic_function(20));
--- server/security_metrics.ts
+++ server/security_metrics.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
-
+import * as formulas from "./formulas"
 const HOURS_UNTIL_NEXT_UPDATE: number = 2;
 const a: number = 0.6;
 const b: number = 1.1;
@@ -12,17 +12,15 @@ interface VersionInfo{
     index: number;
     tagName: string;
     publishDate: Date;
+    scoreRelease : number;
+    scoreIndex: number;
 }
 export default class SecurityMetric {
     listOfVersions: Map<string, VersionInfo>;
     listOfVersionsBuffer: Map<string, VersionInfo>;
-    arrayOfVersions: string[];
-    arrayOfVersionBuffer: string[];
     constructor() {
         this.listOfVersionsBuffer = new Map();
         this.listOfVersions = new Map();
-        this.arrayOfVersions = [];
-        this.arrayOfVersionBuffer = [];
         // this.listOfVersions.set('1.7.2',0);
         // this.listOfVersions.set('1.7.0',1);
         // this.listOfVersions.set('1.6.0',2);
@@ -37,32 +35,23 @@ export default class SecurityMetric {
         }
     }
 
-    rateVersionBasedOnRelease(version: string): number {
-  
-        var index = this.listOfVersions.get(version);
-        if(index!=undefined){
-            if(index.index == 0) return 100;
-            var nextReleaseDate: Date | undefined= this.listOfVersions.get(this.arrayOfVersions[index.index-1])?.publishDate;
-            if(nextReleaseDate==undefined) throw new Error("Unknown error occured");
-
-            var difference = Math.max((((new Date()).getTime()-nextReleaseDate.getTime())/(7*1000*60*60*24))-2, 0);
-            return Math.round((Math.max(-1000, 100-100*(a1*(Math.pow(b1,difference))+c1)) + Number.EPSILON) * 1000) / 1000
+    rateVersionBasedOnRelease(difference: number, evalFunction:(x:number)=>number): number {
+        if(difference>=0){
+            return evalFunction(difference)
             //return index == undefined ? -1000 : Math.max(-1000, 100-100*(a*(Math.pow(b,index.index))+c))
         }else{
             return -1000;
         }
         
     }
-
-    rateVersionBasedOnIndex(version: string):number {
-        if(this.listOfVersions.has(version)){
-            var index = this.listOfVersions.get(version);
-            return index == undefined ? -1000 :  Math.round((Math.max(-1000, 100-100*(a*(Math.pow(b,index.index))+c)) + Number.EPSILON) * 1000) / 1000
-           
-        }else{
-            return -1000;
-        }
+    getRating(version: string){
+        var v = this.listOfVersions.get(version);
+        return v==undefined ? -1000 : (v.scoreIndex, v.scoreRelease)
     }
+    rateVersionBasedOnIndex(index:number, evalFunction: (x:number, a?: number, b?: number, c?: number, cutoff?: number, decimals?: number)=>number):number {
+        return evalFunction(index,a,b,c,-1000,2);
+    }
+
     //Rate Limit 60 requests per hour so be careful!
     checkForUpdate() {
         axios.get(`https://api.github.com/repos/ripple/rippled/releases`,
@@ -72,34 +61,37 @@ export default class SecurityMetric {
             if (res.status == 200) {
                 if (res.data && res.data.length > 0) {
 
-                    if (this,this.arrayOfVersions.length>0 && this.arrayOfVersions[0]==res.data[0].tag_name) {
+                    if (this.listOfVersions && this.listOfVersions.has(res.data[0].tag_name) && this.listOfVersions.get(res.data[0].tag_name)?.index==0) {
+
                         console.log("No update needed");
                         setTimeout(() => this.checkForUpdate(), HOURS_UNTIL_NEXT_UPDATE*60*60 * 1000);
                         return;
                     } else {
+
                         console.log("update needed "+res.data[0].tag_name + " had index "+this.listOfVersions.get(res.data[0]))
                         var i = 0;
                         this.listOfVersionsBuffer.clear();
+                        var prev: VersionInfo | undefined = undefined;
                         res.data.map((tag: any) => {
-                            console.log(tag.tag_name,i)
-                            this.arrayOfVersionBuffer.push(tag.tag_name);
-                            this.listOfVersionsBuffer.set(tag.tag_name, {index: i, publishDate: new Date(tag.published_at), tagName: tag.tag_name});
+                            var difference = 0;
+                            if(prev!=undefined){
+                                difference = Math.max((((new Date()).getTime()-prev.publishDate.getTime())/(7*1000*60*60*24))-2, 0);
+                                
+                            }
+                            
+                            var curr: VersionInfo = {index: i, publishDate: new Date(tag.published_at), tagName: tag.tag_name,
+                                scoreIndex: this.rateVersionBasedOnIndex(i,formulas.power_function),
+                                scoreRelease: this.rateVersionBasedOnRelease(difference, formulas.power_function)};
+                            
+                            this.listOfVersionsBuffer.set(tag.tag_name, curr);
+                            prev=curr;
                             i++;
                         })
-                        //this.updateListOfVersions(2, i);
+                        //this.updateListOfVersions(2, i, prev);
                         this.listOfVersions = this.listOfVersionsBuffer;
-                        this.arrayOfVersions = this.arrayOfVersionBuffer;
                         
-                        console.log(this.listOfVersions);
-                    
-                       // Test stuff: 
-                        // for(var index in this.arrayOfVersions){
-                        //     console.log(this.rateVersionBasedOnRelease(this.arrayOfVersions[index]) + " other metric for "+this.arrayOfVersions[index]+" "+ this.rateVersionBasedOnIndex(this.arrayOfVersions[index]))
-                        //     console.log(index);
-                        // }
-                 
-                        // console.log(this.rateVersionBasedOnIndex('1.0.0av'));
                         setTimeout(() => this.checkForUpdate(), HOURS_UNTIL_NEXT_UPDATE*60*60 * 1000);
+
                         return;
                     }
 
@@ -115,41 +107,52 @@ export default class SecurityMetric {
             console.log("Could not update the list " + err.message);
         });
     }
+
     //The api may not return all the versions. Keep querying until we run out of data.
     //Also the rate limit is 60 requests per hour so be careful.
-    // updateListOfVersions(pageNumber: number, i: number) {
+    updateListOfVersions(pageNumber: number, i: number, prev: VersionInfo) {
 
-    //     axios.get(`https://api.github.com/repos/ripple/rippled/tags`,
-    //         { timeout: 6000, params: { page: pageNumber } }
-    //     ).then((res) => {
+        axios.get(`https://api.github.com/repos/ripple/rippled/tags`,
+            { timeout: 6000, params: { page: pageNumber } }
+        ).then((res) => {
 
-    //         if (res.status == 200) {
-    //             if (res.data && res.data.length > 0) {
-
-    //                 res.data.map((tag: any) => {
-    //                     console.log(tag.name,i)
-    //                     this.listOfVersionsBuffer.set(tag.name, i);
-    //                     i++;
-    //                 })
-    //                 this.updateListOfVersions(pageNumber + 1, i);
+            if (res.status == 200) {
+                if (res.data && res.data.length > 0) {
+                   
+                    res.data.map((tag: any) => {
+                        var difference = 0;
+                            if(prev!=undefined){
+                                difference = Math.max((((new Date()).getTime()-prev.publishDate.getTime())/(7*1000*60*60*24))-2, 0);
+                                
+                            }
+                            
+                            var curr: VersionInfo = {index: i, publishDate: new Date(tag.published_at), tagName: tag.tag_name,
+                                scoreIndex: this.rateVersionBasedOnIndex(i,formulas.power_function),
+                                scoreRelease: this.rateVersionBasedOnRelease(difference, formulas.power_function)};
+                            
+                            this.listOfVersionsBuffer.set(tag.tag_name, curr);
+                            prev=curr;
+                            i++;
+                    })
+                    this.updateListOfVersions(pageNumber + 1, i, prev);
                     
-    //             } else {
+                } else {
 
-    //                 this.listOfVersions = this.listOfVersionsBuffer;
-    //                 console.log(this.listOfVersions);
-    //                 // this.listOfVersions.map((inp) => console.log(inp))
-    //                 setTimeout(() => this.checkForUpdate(), 10 * 1000);
+                    this.listOfVersions = this.listOfVersionsBuffer;
+                    console.log(this.listOfVersions);
+                    // this.listOfVersions.map((inp) => console.log(inp))
+                    setTimeout(() => this.checkForUpdate(), 10 * 1000);
 
-    //             }
-    //         } else {
-    //             console.log("Could not update the list. Status of request: " + res.status);
-    //         }
+                }
+            } else {
+                console.log("Could not update the list. Status of request: " + res.status);
+            }
 
-    //     }).catch((err) => {
-    //         console.log("Could not update the list " + err.message);
+        }).catch((err) => {
+            console.log("Could not update the list " + err.message);
 
-    //     });
-    // }
+        });
+    }
 
 
 
