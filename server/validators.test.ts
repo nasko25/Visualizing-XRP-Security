c1e517d738e02f9417bfd9decfd402f856a954b3
Dimitar Barantiev <D.A.Barantiev@student.tudelft.nl>
2021-06-03 12:26:01 +0300

    Test: Add axios test

--- server/validators.test.ts
+++ server/validators.test.ts
@@ -1,12 +1,11 @@
 import ValidatorIdentifier, { Validator_List_Result, Validator, Validator_Data } from './validators'
-import axios from 'axios';
+import axios, {AxiosResponse} from 'axios';
 import { encode } from 'js-base64';
 
 
 // prepare axios for mocking
 jest.mock("axios");
 const axiosMock = axios as jest.Mocked<typeof axios>;
-
 
 afterEach(() => {
     jest.clearAllMocks();
@@ -82,5 +81,39 @@ test("test: extractValidatorKeys() returns correctly decoded data empty kist", (
     let valIden = new ValidatorIdentifier();
 
     expect(valIden.extractValidatorKeys(valListData)).toEqual(pubKeys);
+
+})
+
+
+test("test: promiseWrapper() successfully returns axios response", async () => {
+
+    // the public keys
+    let pubKeys = ["pubkey1", "pubkey2", "pubkey3"];
+
+    let validators: Validator[] = pubKeys.map(key => <Validator>{manifest: "manifest", validation_public_key: key });
+    let valData: Validator_Data = {
+        validators: validators,
+        expiration: 0,
+        sequence: 1
+    }
+
+    let valListData: Validator_List_Result = {
+        manifest: "manifest",
+        version: 2,
+        public_key: "ddd",
+        signature: "signature",
+        blob: encode(JSON.stringify(valData))
+    };
+
+    let valIden = new ValidatorIdentifier();
+
+    axiosMock.get.mockResolvedValueOnce({data: valListData});
+
+    let public_key: string = "test_key";
+
+    valIden.promiseWrapper("ip", "publisher", public_key).then((res) => {
+        expect(res[0]).toEqual(public_key)
+        expect(res[1]).toEqual(pubKeys)
+    })
 
 })
