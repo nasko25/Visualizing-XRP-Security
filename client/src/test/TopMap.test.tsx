f340226a94e1ae28aa7cb8c8390a106ef084277b
istranski <I.I.Stranski@student.tudelft.nl>
2021-06-13 12:48:56 +0300

    Added a test for selecting a node

--- client/src/test/TopMap.test.tsx
+++ client/src/test/TopMap.test.tsx
@@ -31,7 +31,7 @@ const mockNodeData: Array<any> = [
     {
         IP: '111.222.000.123',
         rippled_version: 'rippled-1.7.0',
-        public_key: 'key',
+        public_key: 'public_key1',
         uptime: 10,
         longtitude: 1.2,
         latitude: 0.3,
@@ -40,7 +40,7 @@ const mockNodeData: Array<any> = [
     {
         IP: '222.111.000.221',
         rippled_version: 'rippled-1.7.1',
-        public_key: 'key2',
+        public_key: 'public_key2',
         uptime: 12,
         longtitude: 1.3,
         latitude: 0.4,
@@ -63,8 +63,8 @@ const mockNodeData: Array<any> = [
 //     expect(map).toBeTruthy();
 // });
 
-test('Correct behavior on render', async () => {
-    const map = await shallow<TopMap>(<TopMap data={mockNodeData} handleChange={() => console.log("node selected")}/>).instance();
+test('Correct behaviour on render', async () => {
+    const map = await shallow<TopMap>(<TopMap data={mockNodeData} handleChange={(public_key: String) => public_key}/>).instance();
 
     const selectNodeSpy = jest.spyOn(map, "selectNode");
     const onClusterClickSpy = jest.spyOn(map, "onClusterClick");
@@ -72,7 +72,7 @@ test('Correct behavior on render', async () => {
     const createNewMapSpy = jest.spyOn(map, "createNewMap");
     const createMarkerGroupSpy = jest.spyOn(map, "createMarkerGroup");
 
-    await map.render();
+    const rendered_map = await map.render();
 
     expect(selectNodeSpy).toHaveBeenCalledTimes(0);
     expect(onClusterClickSpy).toHaveBeenCalledTimes(0);
@@ -80,4 +80,41 @@ test('Correct behavior on render', async () => {
     expect(createNewMapSpy).toHaveBeenCalledTimes(1);
     expect(createMarkerGroupSpy).toHaveBeenCalledTimes(1);
 
+    // Checks whether the keys of the generated map are the correct public keys
+    expect(rendered_map.props.children.props.children[1].props.children[0].props.children.props.children).toEqual("public_key1");
+    expect(rendered_map.props.children.props.children[1].props.children[1].props.children.props.children).toEqual("public_key2");
+
+
+});
+
+test('Selecting a node result in correct behaviour', async() => {
+    const map = await shallow<TopMap>(<TopMap data={mockNodeData} handleChange={(public_key: String) => public_key}/>).instance();
+
+    const selectNodeSpy = jest.spyOn(map, "selectNode");
+    const onClusterClickSpy = jest.spyOn(map, "onClusterClick");
+    const createNewPopupSpy = jest.spyOn(map, "createNewPopup");
+    const createNewMapSpy = jest.spyOn(map, "createNewMap");
+    const createMarkerGroupSpy = jest.spyOn(map, "createMarkerGroup");
+    
+    const rendered_map = await map.render();
+
+    expect(selectNodeSpy).toHaveBeenCalledTimes(0);
+    expect(onClusterClickSpy).toHaveBeenCalledTimes(0);
+    expect(createNewPopupSpy).toHaveBeenCalledTimes(0);
+    expect(createNewMapSpy).toHaveBeenCalledTimes(1);
+    expect(createMarkerGroupSpy).toHaveBeenCalledTimes(1);
+
+    // console.log(rendered_map.props.children.props.children[1].props.children[0].props.eventHandlers.click());
+
+    // For some reason returns undefined instead of the correct public key
+
+    let pub_key = rendered_map.props.children.props.children[1].props.children[0].props.eventHandlers.click();
+
+    expect(selectNodeSpy).toHaveBeenCalledTimes(1);
+    expect(onClusterClickSpy).toHaveBeenCalledTimes(0);
+    expect(createNewPopupSpy).toHaveBeenCalledTimes(0);
+    expect(createNewMapSpy).toHaveBeenCalledTimes(1);
+    expect(createMarkerGroupSpy).toHaveBeenCalledTimes(1);
+
+    // expect(pub_key).toEqual(mockNodeData[0].public_key);
 });
