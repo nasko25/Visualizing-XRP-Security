5ebe18e4f8ca70bc1eb8835086398ef084862f50
istranski <I.Stranski@student.tudelft.nl>
2021-06-20 17:18:15 +0300

    Updated description of security metric on About page

--- client/src/components/about-page/AboutPageMain.tsx
+++ client/src/components/about-page/AboutPageMain.tsx
@@ -49,7 +49,10 @@ export default class AboutPageMain extends React.Component {
                             <h1 style={{ fontSize: "150%", fontWeight: "bold" }}>A note on security</h1>
                             <p>
                                 We have developed a security metric, which measures how secure a particular Node is.
-                                This metric takes into account...(fill in later)
+                                This metric takes into account each node's version and their open pots. 
+                                The older the version the lower the score and likewise - more ports results in lower score. 
+                                The final calculation is a weighted average of the two, normalised between 0 and 100. 
+                                Our findings of open ports and node's version are present on each node's page.
                                 One must look beyond the number.
                             </p>
                         </Box>
